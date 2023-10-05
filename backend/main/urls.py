4e248a079a6a912c96cecaeab55b49d70e5ac48d
Piyush-onGIT <thisissahupiyush@gmail.com>
2023-10-05 09:20:58 +0530

    postgres container set up

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
-from .views import user_registration, user_login, decode_user
+from .views import user_registration, user_login
 
 urlpatterns = [
     path("register", user_registration, name="register-user"),
     path("login", user_login, name="user-login"),
-    path("decode_user", decode_user, name="decode-user"),
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -5,13 +5,7 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserRegistrationSerializer, UserLoginSerializer
-import jwt
-import datetime
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-secret_key = os.environ.get('JWT_KEY')
 
 # registering user
 @api_view(["POST"])
@@ -25,9 +19,12 @@ def user_registration(request):
                 email=serializer.validated_data["email"],
                 password=serializer.validated_data["password"],
             )
+            # generate refresh tokens for user while registration
+            refresh = RefreshToken.for_user(user)
             return Response(
                 {
                     "message": "User registered successfully",
+                    "token": str(refresh.access_token),
                 },
                 status=status.HTTP_201_CREATED,
             )
@@ -36,7 +33,6 @@ def user_registration(request):
 
 
 # logging in user
-
 @api_view(["POST"])
 @permission_classes([AllowAny])
 def user_login(request):
@@ -45,46 +41,27 @@ def user_login(request):
         if serializer.is_valid():
             email = serializer.validated_data["email"]
             password = serializer.validated_data["password"]
-            payload = {
-				"user": email,
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
-			}
-            token = jwt.encode(payload, secret_key, algorithm='HS256')
             try:
                 user = User.objects.get(email=email)
                 if user.password == password:
                     refresh = RefreshToken.for_user(user)
-                    response = Response(
+                    return Response(
                         {
-                            "ok": True,
                             "message": "User logged in successfully",
+                            "token": str(refresh.access_token),
                         },
                         status=status.HTTP_200_OK,
                     )
-                    response.set_cookie("login", token, max_age=3600)
-                    return response
                 else:
                     return Response(
-                        {
-                            "ok": False,
-                            "message": "Invalid login credentials"
-						},
+                        {"message": "Invalid login credentials"},
                         status=status.HTTP_401_UNAUTHORIZED,
                     )
 
             except User.DoesNotExist:
                 return Response(
-                    {"ok": False, "message": "User not found"}, status=status.HTTP_401_UNAUTHORIZED
+                    {"message": "User not found"}, status=status.HTTP_401_UNAUTHORIZED
                 )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
-
-@api_view(["GET"])
-def decode_user(request):
-    token = request.COOKIES.get('login')
-    payload = jwt.decode(token, secret_key, algorithms=['HS256'])
-    response = Response({
-        "ok": True,
-        "message": payload
-	})
-    return response
