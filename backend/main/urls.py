2653c706c62996058d87ff560f2ab1a9ab671572
Piyush-onGIT <thisissahupiyush@gmail.com>
2023-10-05 13:22:52 +0530

    event registration backend

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import user_registration, user_login
+from .views import user_registration, user_login, register_event
 
 urlpatterns = [
     path("register", user_registration, name="register-user"),
     path("login", user_login, name="user-login"),
+    path("register_event", register_event),
 ]
