58163d11b9c45d426e9c8d398b327f9748c49c14
Vibhuti <vibhuti6378@gmail.com>
2023-10-18 12:07:13 +0530

    frontend dashboard for admin done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import EventManagementPage from './components/event_manager';
 import FacultyRegistration from './components/Faculty';
 import Events from './components/Events';
 import HomePage from './components/HomePage';
+import Dashboard_Admin from './components/Dashboard_Admin';
 import './App.css';
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
         <Route path='/register/faculty' element={<FacultyRegistration/>}></Route>
         <Route path='/register/admin' element={<AdminRegistration/>}></Route>
         <Route path='/Event_management' element={<EventManagementPage/>}></Route>
+        <Route path='/DashboardAd' element={<Dashboard_Admin/>}/>
         </Routes>
       </div>
     </BrowserRouter>
