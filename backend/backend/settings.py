7eeedf824b62632148cdda014609581af9275482
Piyush-onGIT <thisissahupiyuh@gmail.com>
2023-10-25 05:35:40 +0530

    all text & sign now are centerd and resized

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -152,6 +152,14 @@ DATABASES = {
 }
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "piyush.at.googl@gmail.com"
+EMAIL_HOST_PASSWORD = "ckcd gegj uqxd gwcg"
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
