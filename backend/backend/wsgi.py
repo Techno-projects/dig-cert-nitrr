807d421b992d513aa4d47d3a723631c707a60d7a
Pratham Gandhi <ppg.1382@gmail.com>
2024-02-18 22:15:04 +0530

    Moved settings out from the module

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -12,8 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 if os.environ.get('PROD'): 
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.production')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.production')
 else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.dev')
 
 application = get_wsgi_application()
--- backend/manage.py
+++ backend/manage.py
@@ -7,9 +7,9 @@ import sys
 def main():
     """Run administrative tasks."""
     if os.environ.get('PROD'): 
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.production')
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.production')
     else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.dev')
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
