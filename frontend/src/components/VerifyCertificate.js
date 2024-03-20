775b9476b5d194acaa31734c452a5b7c1bb8988a
Pratham Gandhi <ppg.1382@gmail.com>
2024-03-20 23:44:31 +0530

    Added a new download button. On first render a low res jpeg image will be displayed. A high res png image would be available for downloading on clicking the download button

--- frontend/src/components/VerifyCertificate.js
+++ frontend/src/components/VerifyCertificate.js
@@ -9,9 +9,11 @@ const server = urls.SERVER_URL;
 const VerifyCertificate = () => {
   const [serial, setSerial] = useState("");
   const [loading, setLoading] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  const [downloading, setDownloading] = useState(false);
   // const [pdfGenerated, setPdfGenerated] = useState(false);
 //   const [base64Image, setBase64Image] = useState("");
-  const [imageSrc, setImageSrc] = useState("");
+  const [imageSrc, setImageSrc] = useState(null);
   
   const handleSerialChange = (event) => {
     setSerial(event.target.value);
@@ -27,11 +29,38 @@ const VerifyCertificate = () => {
     //   setBase64Image(`data:application/png;base64,${res.data.certificate}`);
     }
     catch (error) {
+      setLoaded(false);
+      setImageSrc(null);
       alert("Certificate not found");
     }
     finally {
       setLoading(false);
+      setLoaded(true);
     } 
+  }
+
+  const handleDownload = async () => {
+    try {
+      setDownloading(true);
+      const response = await axios.get(`${server}/api/get_certificate?serial=${serial}&download=true`, { responseType: "blob" } )
+      const blob = new Blob([response.data], { type: response.headers['content-type'] });
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download',`${serial}.png`);
+
+      document.body.appendChild(link);
+      link.click();
+
+      window.URL.revokeObjectURL(url);
+      document.body.removeChild(link);
+    }
+    catch (error) {
+      alert("Certificate could not be downloaded");
+    }
+    finally {
+      setDownloading(false);
+    }
   }
 
   return (
@@ -49,12 +78,19 @@ const VerifyCertificate = () => {
             onChange={handleSerialChange}
             style={{ marginBottom: '10px' }}
           />
-          <button onClick={handleVerifyCertificate} disabled={loading} className="handleVerifyButton">
+          <button onClick={handleVerifyCertificate} disabled={loading} className="VerifyButton">
             {loading ? 'Loading...' : 'Get Certificate'}
           </button>
         </center>
-        <div style={{ height: '60vh', width: '60vw', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          {imageSrc && <img src={imageSrc} style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'contain' }} />}
+        <div style={{ height: '60vh', width: '60vw', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '50px' }}>
+          {imageSrc && loaded && (
+            <>
+              <img src={imageSrc} style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'contain' }} />
+              <button onClick={handleDownload} className="VerifyButton" style={{ marginTop: "20px" }}>
+                {downloading ? 'Downloading...' : 'Download'}
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
