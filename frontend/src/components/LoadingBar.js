c7496832405ca7872deacc4366f9301f82ae19e8
probot-codes <ps25082005@gmail.com>
2025-02-27 14:19:00 +0530

    Loading Bar Added

---
+++ frontend/src/components/LoadingBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/LoadingBar.css';
+
+const LoadingBar = () => (
+  <div className="loading-bar-container">
+    <div className="loading-bar" />
+  </div>
+);
+
+export default LoadingBar;
